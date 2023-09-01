479d7540860ea29cfa2c5f779342b090606d3a2a
Selma <shrustic1@etf.unsa.ba>
2023-09-01 23:16:43 +0200

    added private method createConnection in AsbtractDao class

--- src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
@@ -1,4 +1,37 @@
 package ba.unsa.etf.rpr.dao;
 
-public class AbstractDao {
+import ba.unsa.etf.rpr.domain.Idable;
+
+import java.sql.*;
+import java.util.Properties;
+
+public abstract class AbstractDao<T extends Idable> implements Dao<T>{
+    private static Connection conn = null;
+    private String tableName;
+    private static void createConnection(){
+        if (conn == null){
+            try {
+                Properties p = new Properties();
+                p.load(ClassLoader.getSystemResource("application.properties.sample").openStream());
+                String url = p.getProperty("db.connection_string");
+                String username = p.getProperty("db.username");
+                String password = p.getProperty("db.password");
+                conn = DriverManager.getConnection(url,username,password);
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally{
+                Runtime.getRuntime().addShutdownHook(new Thread(){
+                    @Override
+                    public void run(){
+                        try {
+                            conn.close();
+                        } catch (SQLException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+            }
+        }
+    }
+
 }
