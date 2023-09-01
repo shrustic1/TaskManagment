229771fe4c70b513db1ffa632096ba161f513dfe
Selma <shrustic1@etf.unsa.ba>
2023-09-01 22:23:52 +0200

    added private attributes in User class

--- src/main/java/ba/unsa/etf/rpr/domain/User.java
+++ src/main/java/ba/unsa/etf/rpr/domain/User.java
@@ -1,4 +1,9 @@
 package ba.unsa.etf.rpr.domain;
 
 public class User {
+    private int id;
+    private String name;
+    private String surname;
+    private String email;
+    private String password;
 }
