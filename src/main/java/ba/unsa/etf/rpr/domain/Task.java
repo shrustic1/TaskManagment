781e4fe91a615a488c81c93056605ecb4dbf4d02
Selma <shrustic1@etf.unsa.ba>
2023-09-01 22:30:25 +0200

    added private attributes in Task class

--- src/main/java/ba/unsa/etf/rpr/domain/Task.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Task.java
@@ -1,4 +1,15 @@
 package ba.unsa.etf.rpr.domain;
 
+import java.util.Date;
+
 public class Task {
+    private int id;
+    private String title;
+    private String description;
+    private Date date;
+    private Date deadline;
+    private String status;
+    private int reported_id;
+    private int assignee_id;
+    private int tag_id;
 }
