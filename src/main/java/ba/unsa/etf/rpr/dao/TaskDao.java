b21805dbcec05dd3a7aa695b80892964446b8a48
Selma <shrustic1@etf.unsa.ba>
2023-09-01 22:58:56 +0200

    added searchByDeadline in TaskDao

--- src/main/java/ba/unsa/etf/rpr/dao/TaskDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/TaskDao.java
@@ -1,12 +1,16 @@
 package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Task;
+import ba.unsa.etf.rpr.domain.User;
 import ba.unsa.etf.rpr.exceptions.MyException;
 
+import java.util.Date;
 import java.util.List;
 
 public interface TaskDao extends Dao<Task>{
 
     List<Task> searchByStatus(String status) throws MyException;
     List<Task> searchByTitle(String title) throws MyException;
+    List<Task> searchByDeadline(Date deadline) throws MyException;
+
 }
