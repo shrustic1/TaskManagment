b1e4cc023d760e2211b1e785aedfd37d4a994bf8
Selma <shrustic1@etf.unsa.ba>
2023-09-02 00:17:34 +0200

    added private static final Dao's for every table

--- src/main/java/ba/unsa/etf/rpr/dao/DaoFactory.java
+++ src/main/java/ba/unsa/etf/rpr/dao/DaoFactory.java
@@ -1,4 +1,8 @@
 package ba.unsa.etf.rpr.dao;
 
 public class DaoFactory {
+    private static final UserDao userDao = UserDaoSQLImpl.getInstance();
+    private static final TaskDao taskDao = TaskDaoSQLImpl.getInstance();
+    private static final TagDao tagDao = TagDaoSQLImpl.getInstance();
+
 }
