c8d79fe04b6f9926302fb34b9680ba4a113bfd19
Selma <shrustic1@etf.unsa.ba>
2023-09-01 23:20:25 +0200

    added two constructors in MyException

--- src/main/java/ba/unsa/etf/rpr/exceptions/MyException.java
+++ src/main/java/ba/unsa/etf/rpr/exceptions/MyException.java
@@ -1,5 +1,11 @@
 package ba.unsa.etf.rpr.exceptions;
 
 public class MyException extends Exception{
-    
+    public MyException(String msg, Exception reason){
+        super(msg, reason);
+    }
+
+    public MyException(String msg){
+        super(msg);
+    }
 }
