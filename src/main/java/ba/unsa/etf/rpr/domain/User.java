9fa687ca6b3eeb401f4a494623de512c9c589f9e
Selma <shrustic1@etf.unsa.ba>
2023-09-01 22:30:55 +0200

    added getters in User class

--- src/main/java/ba/unsa/etf/rpr/domain/User.java
+++ src/main/java/ba/unsa/etf/rpr/domain/User.java
@@ -6,4 +6,24 @@ public class User {
     private String surname;
     private String email;
     private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
