c41a8504538e9a44de5147992054d2ce27c1b2ed
Selma <shrustic1@etf.unsa.ba>
2023-09-01 23:23:03 +0200

    added method delete in AbstractDao

--- src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
@@ -100,4 +100,14 @@ public abstract class AbstractDao<T extends Idable> implements Dao<T>{
         }
         return columns.toString();
     }
+    public void delete(int id) throws MyException{
+        String sql = "DELETE FROM "+tableName+" WHERE id = ?";
+        try {
+            PreparedStatement stmt = getConnection().prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            stmt.setObject(1, id);
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new MyException(e.getMessage(), e);
+        }
+    }
 }
