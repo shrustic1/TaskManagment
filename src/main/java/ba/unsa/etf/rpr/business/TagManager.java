57b334e7cccbd866970ebdd93d68bffab98e4eb5
Selma <shrustic1@etf.unsa.ba>
2023-09-02 01:19:38 +0200

    added method add in TagManager

---
+++ src/main/java/ba/unsa/etf/rpr/business/TagManager.java
@@ -0,0 +1,11 @@
+package ba.unsa.etf.rpr.business;
+
+import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.exceptions.MyException;
+import ba.unsa.etf.rpr.domain.Tag;
+
+public class TagManager {
+    public void add (Tag tag) throws MyException {
+        DaoFactory.tagDao().add(tag);
+    }
+}
