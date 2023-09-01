f4d9f35fdaf64a8b06c4f18f1131764a2eeee28b
Selma <shrustic1@etf.unsa.ba>
2023-09-02 01:31:32 +0200

    created Options in App

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -1,11 +1,35 @@
 package ba.unsa.etf.rpr;
 
-/**
- * Hello world!
- *
- */
+import ba.unsa.etf.rpr.business.TagManager;
+import ba.unsa.etf.rpr.business.UserManager;
+import ba.unsa.etf.rpr.business.TaskManager;
+import ba.unsa.etf.rpr.domain.Tag;
+import ba.unsa.etf.rpr.domain.User;
+import ba.unsa.etf.rpr.domain.Task;
+import ba.unsa.etf.rpr.exceptions.MyException;
+import org.apache.commons.cli.*;
+
+import java.io.PrintWriter;
+import java.util.List;
+
 public class App 
 {
+    private static final UserManager cookManager= new UserManager();
+    private static final TaskManager studentManager = new TaskManager();
+    private static final TagManager mealManager = new TagManager();
+
+    private static final Option addTag = new Option("AddTag","add-tag",false, "Adding new tag to database");
+    private static final Option addSUser = new Option("AddU","add-user",false, "Adding new user to database");
+    private static final Option addTask = new Option("AddT", "add-task", false, "Adding new task to database");
+
+    private static final Option getTags = new Option("GetTag","get-tags",false, "Printing all tags from database");
+    private static final Option getUsers = new Option("GetU","get-users",false, "Printing all users from database");
+    private static final Option getTasks = new Option("GetT", "get-tasks", false, "Printing all tasks from database");
+
+    private static final Option deleteTag = new Option("deleteTag","delete-tag",false,"Deletes a tag from database");
+    private static final Option deleteUsers = new Option("deleteU","delete-user",false,"Deletes an user from database");
+    private static final Option deleteTask = new Option("deleteT","delete-task",false,"Deletes a task from database");
+
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
