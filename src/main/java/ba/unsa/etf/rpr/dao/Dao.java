157f5fa25e62c0bc2b2e22635596637b36ba536e
Selma <shrustic1@etf.unsa.ba>
2023-09-01 22:47:39 +0200

    modified method getAll in Dao interface

--- src/main/java/ba/unsa/etf/rpr/dao/Dao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/Dao.java
@@ -2,9 +2,12 @@ package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.exceptions.MyException;
 
+import java.util.List;
+
 public interface Dao<T> {
     T getById(int id) throws MyException;
     T add(T item) throws MyException;
     T update(T item) throws MyException;
     void delete(int id) throws MyException;
+    List<T> getAll() throws MyException;
 }
