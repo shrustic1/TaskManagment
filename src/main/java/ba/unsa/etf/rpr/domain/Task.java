ead5a8770cac376d65fd0187025b9432a9c678df
Selma <shrustic1@etf.unsa.ba>
2023-09-01 22:32:20 +0200

    added getters in Task class

--- src/main/java/ba/unsa/etf/rpr/domain/Task.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Task.java
@@ -12,4 +12,40 @@ public class Task {
     private int reported_id;
     private int assignee_id;
     private int tag_id;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public Date getDeadline() {
+        return deadline;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public int getReported_id() {
+        return reported_id;
+    }
+
+    public int getAssignee_id() {
+        return assignee_id;
+    }
+
+    public int getTag_id() {
+        return tag_id;
+    }
 }
