aa3aa994bcc658ecd8e38ac2894f3eaa4b2cf587
Selma <shrustic1@etf.unsa.ba>
2023-09-02 00:00:33 +0200

    overriden method row2object in AbstractDao

--- src/main/java/ba/unsa/etf/rpr/dao/TagDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/TagDaoSQLImpl.java
@@ -1,4 +1,29 @@
 package ba.unsa.etf.rpr.dao;
 
-public class TagDaoSQLImpl {
+import ba.unsa.etf.rpr.domain.Tag;
+import ba.unsa.etf.rpr.exceptions.MyException;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class TagDaoSQLImpl extends AbstractDao<Tag> implements TagDao{
+    private static TagDaoSQLImpl instance = null;
+    private TagDaoSQLImpl(){super("Tags");}
+
+    public static TagDao getInstance() {
+        if (instance == null) instance = new TagDaoSQLImpl();
+        return instance;
+    }
+
+    @Override
+    public Tag row2object(ResultSet rs) throws MyException {
+        try {
+            Tag tag = new Tag();
+            tag.setId(rs.getInt("id"));
+            tag.setName(rs.getString("name"));
+            return tag;
+        } catch (SQLException e) {
+            throw new MyException(e.getMessage(), e);
+        }
+    }
 }
