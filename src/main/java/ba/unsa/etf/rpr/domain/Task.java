c1263a62b2adeefec65f8b720380d04c426a7ba6
Selma <shrustic1@etf.unsa.ba>
2023-09-01 22:32:44 +0200

    added setters in Task class

--- src/main/java/ba/unsa/etf/rpr/domain/Task.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Task.java
@@ -48,4 +48,40 @@ public class Task {
     public int getTag_id() {
         return tag_id;
     }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public void setDeadline(Date deadline) {
+        this.deadline = deadline;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setReported_id(int reported_id) {
+        this.reported_id = reported_id;
+    }
+
+    public void setAssignee_id(int assignee_id) {
+        this.assignee_id = assignee_id;
+    }
+
+    public void setTag_id(int tag_id) {
+        this.tag_id = tag_id;
+    }
 }
