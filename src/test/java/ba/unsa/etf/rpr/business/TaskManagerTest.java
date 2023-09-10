75069afccc19b02833139c7ba0e5b5f610d9d2d6
Selma <shrustic1@etf.unsa.ba>
2023-09-11 01:25:01 +0200

    added @Test searchTasksTest in TaskManagerTest

--- src/test/java/ba/unsa/etf/rpr/business/TaskManagerTest.java
+++ src/test/java/ba/unsa/etf/rpr/business/TaskManagerTest.java
@@ -19,5 +19,25 @@ public class TaskManagerTest {
     private Task task;
     private TaskDaoSQLImpl taskDaoSQLMock;
     private List<Task> tasksList;
+    @Test
+    void searchTasksTest() throws MyException {
+        when(taskManager.getAll()).thenReturn(tasksList);
+        String string = "kupiti mlijeko";
+        Mockito.doCallRealMethod().when(taskManager).searchTasks(string);
+        List<Task> tasks = taskManager.searchTasks(string);
+        assertAll(
+                "Searching task",
+                () -> assertEquals(1 , tasks.size()),
+                () -> assertEquals("kupiti mlijeko" , tasks.get(0).getTitle()),
+                () -> assertEquals("kupiti cetiri litra",tasks.get(0).getDescription()),
+                () -> assertEquals(7,tasks.get(0).getId()),
+                () -> assertEquals("2023-03-06 22:00:00", tasks.get(0).getDate()),
+                () -> assertEquals("2023-03-10 22:00:00", tasks.get(0).getDeadline()),
+                () -> assertEquals("none", tasks.get(0).getStatus()),
+                () -> assertEquals("Selma", tasks.get(0).getAssignee()),
+                () -> assertEquals("Hanka", tasks.get(0).getReporter()),
+                () -> assertEquals("Kupovina", tasks.get(0).getTag())
+        );
 
+    }
 }
