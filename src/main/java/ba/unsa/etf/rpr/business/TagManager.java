ff61db117e8a0b5d43d518d67e4a6f167ef71472
Selma <shrustic1@etf.unsa.ba>
2023-09-11 00:24:41 +0200

    added method validateTagName in TagManager

--- src/main/java/ba/unsa/etf/rpr/business/TagManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/TagManager.java
@@ -1,22 +1,32 @@
 package ba.unsa.etf.rpr.business;
 
 import ba.unsa.etf.rpr.dao.DaoFactory;
-import ba.unsa.etf.rpr.exceptions.MyException;
 import ba.unsa.etf.rpr.domain.Tag;
+import ba.unsa.etf.rpr.exceptions.MyException;
 
 import java.util.List;
 
 public class TagManager {
-    public void add (Tag tag) throws MyException {
-        DaoFactory.tagDao().add(tag);
+
+    public List<Tag> getAll() throws MyException {
+        return DaoFactory.tagDao().getAll();
     }
-    public void delete(int id) throws MyException{
+
+    public void delete(int id) throws MyException {
         DaoFactory.tagDao().delete(id);
     }
-    public void update(Tag tag) throws MyException{
-        DaoFactory.tagDao().update(tag);
+
+    public Tag update(Tag tag) throws MyException {
+        return DaoFactory.tagDao().update(tag);
     }
-    public List<Tag> getAll() throws MyException{
-        return DaoFactory.tagDao().getAll();
+
+    public Tag add(Tag tag) throws MyException {
+        return DaoFactory.tagDao().add(tag);
+    }
+
+    public void validateTagName(String name) throws MyException{
+        if (name == null || name.length() > 45 || name.length() < 3){
+            throw new MyException("Tag name must be between 3 and 45 chars");
+        }
     }
 }
