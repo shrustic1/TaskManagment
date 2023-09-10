4f4b70ae6a910255650f6d3c29707eba4fb6c8df
Selma <shrustic1@etf.unsa.ba>
2023-09-11 00:18:04 +0200

    added @BeforeEach in TagManagerTest

--- src/test/java/ba/unsa/etf/rpr/business/TagManagerTest.java
+++ src/test/java/ba/unsa/etf/rpr/business/TagManagerTest.java
@@ -2,7 +2,11 @@ package ba.unsa.etf.rpr.business;
 
 import ba.unsa.etf.rpr.dao.TagDaoSQLImpl;
 import ba.unsa.etf.rpr.domain.Tag;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.Mockito;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class TagManagerTest {
@@ -10,4 +14,18 @@ public class TagManagerTest {
     private Tag tag;
     private TagDaoSQLImpl tagDaoSQLMock;
     private List<Tag> tags;
+    /**
+     * This method will be called before each test method
+     */
+    @BeforeEach
+    public void initializeObjects() {
+        tagManager = Mockito.mock(TagManager.class);
+        tag = new Tag();
+        tag.setName("kupovina");
+        tag.setId(50);
+
+        tagDaoSQLMock = Mockito.mock(TagDaoSQLImpl.class);
+        tags = new ArrayList<>();
+        tags.addAll(Arrays.asList(new Tag("kupovina"), new Tag("hitno"), new Tag("vazno")));
+    }
 }
