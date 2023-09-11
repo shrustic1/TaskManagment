15bc4186aa50285fcc681aa9236705e2b33ce9b8
Selma <shrustic1@etf.unsa.ba>
2023-09-11 07:37:58 +0200

    created LoginController

---
+++ src/main/java/ba/unsa/etf/rpr/controllers/LoginController.java
@@ -0,0 +1,22 @@
+package ba.unsa.etf.rpr.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
+
+public class LoginController {
+    public TextField fieldUsername;
+    @FXML
+    public void initialize(){
+    }
+
+}
