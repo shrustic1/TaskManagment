a112ccd0458607f53a628d5e4c4bc96142fd08e6
Selma <shrustic1@etf.unsa.ba>
2023-09-11 08:45:40 +0200

    created static final instances of Option for deleting a Tag, an User, a Task

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -28,6 +28,11 @@ public class App
     private static final Option getTags = new Option("GetTag","get-tags",false, "Printing all tags from database");
     private static final Option getUsers = new Option("GetU","get-users",false, "Printing all users from database");
     private static final Option getTasks = new Option("GetT", "get-tasks", false, "Printing all tasks from database");
-    
+
+    private static final Option deleteTag = new Option("deleteTag","delete-tag",false,"Deletes a tag from database");
+    private static final Option deleteUser = new Option("deleteU","delete-user",false,"Deletes an user from database");
+    private static final Option deleteTask = new Option("deleteT","delete-task",false,"Deletes a task from database");
+
+
 
 }
