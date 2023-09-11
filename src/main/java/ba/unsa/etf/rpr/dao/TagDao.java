553c42d01a046cf14e644a3913394a843e3339b6
Selma <shrustic1@etf.unsa.ba>
2023-09-11 07:36:35 +0200

    modified

--- src/main/java/ba/unsa/etf/rpr/dao/TagDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/TagDao.java
@@ -1,10 +1,6 @@
 package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Tag;
-import ba.unsa.etf.rpr.exceptions.MyException;
 
-import java.util.List;
-
-public interface TagDao extends Dao<Tag>{
-
+public interface TagDao extends Dao<Tag> {
 }
