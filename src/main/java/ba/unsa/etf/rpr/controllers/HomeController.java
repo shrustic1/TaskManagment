994af0670985e9239f705caf8b14feb65dc27bd7
Selma <shrustic1@etf.unsa.ba>
2023-09-11 07:34:17 +0200

    created HomeController

---
+++ src/main/java/ba/unsa/etf/rpr/controllers/HomeController.java
@@ -0,0 +1,20 @@
+package ba.unsa.etf.rpr.controllers;
+
+import ba.unsa.etf.rpr.exceptions.MyException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
+
+public class HomeController {
+    @FXML
+    public void initialize(){
+    }
+
+}
