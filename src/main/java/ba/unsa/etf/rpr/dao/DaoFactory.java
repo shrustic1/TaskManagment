0e48e96cd576272e064bad8641e49239728414e2
Selma <shrustic1@etf.unsa.ba>
2023-09-11 07:36:20 +0200

    modified

--- src/main/java/ba/unsa/etf/rpr/dao/DaoFactory.java
+++ src/main/java/ba/unsa/etf/rpr/dao/DaoFactory.java
@@ -1,12 +1,25 @@
 package ba.unsa.etf.rpr.dao;
 
+import java.util.List;
+
 public class DaoFactory {
+
+    private static final TagDao tagDao = TagDaoSQLImpl.getInstance();
     private static final UserDao userDao = UserDaoSQLImpl.getInstance();
     private static final TaskDao taskDao = TaskDaoSQLImpl.getInstance();
-    private static final TagDao tagDao = TagDaoSQLImpl.getInstance();
+    private DaoFactory(){
+    }
 
-    private DaoFactory(){}
-    public static TagDao tagDao(){return tagDao;}
-    public static UserDao userDao(){return userDao;}
-    public static TaskDao taskDao(){return taskDao;}
+    public static TagDao tagDao(){
+        return tagDao;
+    }
+
+    public static UserDao userDao(){
+        return userDao;
+    }
+
+    public static TaskDao taskDao(){
+        return taskDao;
+    }
+
 }
